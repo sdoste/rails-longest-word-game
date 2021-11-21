d2200b9c3d9d99820cbbc85883ff5b425be727ab
Sergio Doste <sergiodoste@gmail.com>
2021-11-21 16:35:39 +0100

    reinstall pc update

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,6 @@
+require 'open-uri' # Allows us to send GET requests and receive the response
+require 'json'  # Allows us to parse the reponse into a JSON object/hash
+
 class GamesController < ApplicationController
   def new
     @letters = []
@@ -10,13 +13,21 @@ class GamesController < ApplicationController
 
   def score
     @letters = params[:letters].split(' ')
-    @userinput = params[:userinput].split(' ')
+    @userinput = params[:userinput].split('')
+    word_exists = "<b>Congratulations!</b> #{@userinput.join('').upcase} is a valid English word!".html_safe
+    word_doesnt_exist = "Sorry but #{@userinput.join('').upcase} does not seem to be a valid English word..."
+    word_cant_be_formed = "Sorry but #{@userinput.join('').upcase} can't be built out of #{@letters.join(', ').upcase}"
     @userinput.each do |letter|
-      if @userinput.include? letter
-        @answer = "well now i have to check if it exists or not"
-      else
-        @answer = "Sorry but #{letter} can't be built out of #{@letters}"
-      end
+      # Letters not contained
+      return word_cant_be_formed unless @letters.include? letter
+
+      @letters.delete_at(@letters.index(letter))
     end
+    # Letters are contained in array
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{@userinput.join('')}").read
+    json = JSON.parse(response)
+    # Word exists if found = true, doesnt exist if false
+    json['found'] ? word_exists : word_doesnt_exist
   end
+  helper_method :score
 end
